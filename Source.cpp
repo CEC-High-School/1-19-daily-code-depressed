e3c3545456abe44523ae7d61e5145e6b9b7490c1
turbo808p <35495496+turbo808p@users.noreply.github.com>
2018-01-19 08:06:06 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,42 +1,14 @@
+#include <iostream> //libary contains prewritten code
+using namespace std; //like a cheat code less typing 
+
+int main() { //the starting point of your program
+	int age;
+	cout << "How old are you?" << endl;
+	cin >> age;
+	cout << "Haha you are " << age << " years old" << endl;
 
 
-//instructions for 1-19
-//
-//intro: Write a program that asks a user for how old they are, and then repeats that number back to them in an insult.
-//
-//intermediate: Write a program that prints the numbers 256 down to 32, counting by fours
-//
-//advanced: make an account at HackerRank.com
-//do this problem: https://www.hackerrank.com/challenges/c-tutorial-struct/problem
 
-//CODE IS HERE//
-#include <iostream>
-#include <sstream>
-using namespace std;
 
-/*
-Enter code for class Student here.
-Read statement for specification.
-*/
 
-int main() {
-	int age, standard;
-	string first_name, last_name;
-
-	cin >> age >> first_name >> last_name >> standard;
-
-	Student st;
-	st.set_age(age);
-	st.set_standard(standard);
-	st.set_first_name(first_name);
-	st.set_last_name(last_name);
-
-	cout << st.get_age() << "\n";
-	cout << st.get_last_name() << ", " << st.get_first_name() << "\n";
-	cout << st.get_standard() << "\n";
-	cout << "\n";
-	cout << st.to_string();
-
-	return 0;
 }
-
